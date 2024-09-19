b6d7f956fe5b1f51b94af6ffb089840635eac031
abdullah107189 <abdullah107189@gmail.com>
2024-09-19 14:43:19 +0600

    account.html problem

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ loginBtn.addEventListener('click', (e) => {
     const inputNumber = parseFloat(document.getElementById('input-number').value)
     const inputPin = parseFloat(document.getElementById('input-pin').value)
     if (inputNumber === 5 && inputPin === 1234){
-        window.location.href = '/account.html'
+        window.location.href = 'account.html'
     }
     else{
         alert('Who are you?')
