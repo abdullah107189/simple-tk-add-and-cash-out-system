4116efe27f93c35e4a1bc86c13263293ead62360
abdullah107189 <abdullah107189@gmail.com>
2024-09-19 13:42:23 +0600

    first commit

---
+++ js/account.js
@@ -0,0 +1,40 @@
+const addAmmountBtn = document.getElementById('addAmmountBtn')
+const cashOutBtn = document.getElementById('cashOutBtn')
+
+const addAmmountForm = document.getElementById('addAmmountForm')
+const cashOutForm = document.getElementById('cashOutForm')
+cashOutBtn.addEventListener('click', () => {
+    addAmmountForm.classList.add('hidden')
+    cashOutForm.classList.remove('hidden')
+})
+addAmmountBtn.addEventListener('click', () => {
+    addAmmountForm.classList.remove('hidden')
+    cashOutForm.classList.add('hidden')
+})
+
+const resultCashOutBtn = document.getElementById('resultCashOutBtn')
+const inputCashAmmount = parseFloat(document.getElementById('input-cash-ammount').value)
+const inputCashPin = parseFloat(document.getElementById('input-cash-pin').value)
+
+
+const resultAddAmmount = document.getElementById('resultAddAmmount')
+const mainBalanceString = document.getElementById('mainBalance')
+const mainBalance = parseFloat(mainBalanceString.innerText)
+
+resultAddAmmount.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const inputAddAmmount = parseFloat(document.getElementById('input-add-ammount').value)
+    const inputAddPin = parseFloat(document.getElementById('input-add-pin').value)
+
+    if (inputAddPin === 1234) {
+        const addTotal = (mainBalance + inputAddAmmount)
+        console.log('total', addTotal)
+        mainBalanceString.innerText = addTotal
+        document.getElementById('input-add-ammount').value = ''
+        document.getElementById('input-add-pin').value = ''
+    }
+    else {
+        alert('please enter correct pin number')
+    }
+})
---
+++ js/login.js
@@ -0,0 +1,12 @@
+const loginBtn = document.getElementById('loginBtn')
+loginBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const inputNumber = parseFloat(document.getElementById('input-number').value)
+    const inputPin = parseFloat(document.getElementById('input-pin').value)
+    if (inputNumber === 5 && inputPin === 1234){
+        window.location.href = '/account.html'
+    }
+    else{
+        alert('Who are you?')
+    }
+})
